3f8076b47d6484326eddcd2a69cccf999e93d0af
Luis181996 <luishumberto181996@hotmail.com>
2023-10-01 15:34:26 -0500

    fragments

---
+++ src/test/java/cibertec/edu/pe/appwebfragments2/AppWebFragments2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appwebfragments2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragments2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
